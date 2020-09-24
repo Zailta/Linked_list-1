c082e216413f8a1b39ca75225bbbad7aeb734a3b
Zailta <mannanbhat60@gmail.com>
2020-09-24 14:21:07 +0530

    Insertion at begenning of a linked list

--- Source1.cpp
+++ Source1.cpp
@@ -21,3 +21,15 @@ void Create(Node** start, int new_data) {
     new_node->next = *start;
     return;
 }
+
+void Insert_beg(Node** start, int new_data) {
+    Node* new_node = new Node();
+    new_node->data = new_data;
+    Node* ptr;
+    while (ptr->next != *start)
+        ptr = ptr->next;
+    ptr->next = new_node;
+    new_node->next = *start;
+    *start = new_node;
+    return;
+}
