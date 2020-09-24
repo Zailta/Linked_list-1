00c47aec8403a30364978a8df975a40ec8959b93
Zailta <mannanbhat60@gmail.com>
2020-09-24 14:16:55 +0530

    Creation of a circular linked list

--- Source1.cpp
+++ Source1.cpp
@@ -1,9 +1,16 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-
 class Node {
 public:
     int data;
     Node* next;
 };
+void Create(Node** start, int new_data) {
+    Node* new_node = new Node();
+    Node* ptr;
+    new_node->data = new_data;
+    if (*start == NULL) {
+        new_node->next = new_node;
+        *start = new_node;
+        return;
+    }
